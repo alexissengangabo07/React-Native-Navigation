d2cda5ad6ecd05eb96051440ee79edaa06177b94
alexsenga <alexxngabo@gmail.com>
2022-06-04 16:51:53 +0200

    feat: adding content

--- App.js
+++ App.js
@@ -1,11 +1,58 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { 
+  View, Text, SafeAreaView, ScrollView, StatusBar, 
+  Image, Button
+} from 'react-native';
+import style from './style'; 
 
 function App() {
   return (
-    <View>
-      <Text>Hello World!</Text>
-    </View>
+    <SafeAreaView style={style.container}>
+      <StatusBar barStyle={"light-content"} hidden={false} backgroundColor="#00BCD4"  />
+      <ScrollView>
+        <View>
+        <Text>Treding</Text>
+        <View class="trending annonces">
+            <View style={style.trendingItems}>
+                <View>
+                    <Image source={require("./images/img-1-new.png")} style={style.imgSmall} />
+                </View>
+                <View>
+                    <Text class="username">@tegan</Text>
+                    <Text class="trending-detail">World Peace Builder</Text>
+                </View>
+            </View>
+            <View style={style.trendingItems}>
+                <View>
+                    <Image source={require("./images/img-1.png")} style={style.imgSmall} />
+                </View>
+                <View>
+                    <Text class="username">@morgan</Text>
+                    <Text class="trending-detail">Super Cool Project</Text>
+                </View>
+            </View>
+            <View style={style.trendingItems}>
+                <View>
+                    <Image source={require("./images/img-3.png")} style={style.imgSmall} />
+                </View>
+                <View>
+                    <Text class="username">@kendall</Text>
+                    <Text class="trending-detail">Life Changing App</Text>
+                </View>
+            </View>
+            <View style={style.trendingItems}>
+                <View>
+                    <Image source={require("./images/img-4.png")} style={style.imgSmall} />
+                </View>
+                <View>
+                    <Text class="username">@alex</Text>
+                    <Text class="trending-detail">No Traffic Maker</Text>
+                </View>
+            </View>
+        </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
---
+++ style.js
@@ -0,0 +1,22 @@
+import {StyleSheet} from 'react-native';
+
+const style = StyleSheet.create({
+    container: {
+      backgroundColor: 'white'
+    },
+    text: {
+      color: 'white'
+    },
+    imgSmall: {
+        height: 100,
+        width: 100,
+        borderRadius: 50
+    },
+    trendingItems: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center'
+    }
+  });
+
+  export default style;
